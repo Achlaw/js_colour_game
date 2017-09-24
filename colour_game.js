00f9cad45e9981096aeb9178483ec6e8a40badb7
Alex Law <achlawuk@gmail.com>
2017-09-24 12:43:09 +0100

    Generate 6 random colours in array

--- colour_game.js
+++ colour_game.js
@@ -1,12 +1,4 @@
-var colours = [
-  "rgb(255, 0, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(255, 0, 255)"
-]
-
+var colours = generateRandomColour(6);
 var squares = document.querySelectorAll(".square");
 var choosenColour = chooseColour();
 var colourDisplay = document.querySelector("#colourDisplay");
@@ -26,6 +18,21 @@ for(var i = 0; i < squares.length; i++){
       messageDisplay.textContent = "Try again!";
     }
   });
+}
+
+function generateRandomColour(num){
+  var arr = [];
+  for(var i = 0; i < num; i++){
+    arr.push(randomColour());
+  }
+  return arr;
+}
+
+function randomColour(){
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 function chooseColour(){
